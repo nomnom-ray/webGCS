c014935ebc76c94d5fcede4eb32d4ad73d54cc11
ray <raythilist@gmail.com>
2018-03-12 03:39:25 -0400

    fixed  lot crossover in js; selectable with GCS output

--- templates/index.js
+++ templates/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() { 
-    
+
     var conn;
     var log = $("#log");
     var pixelX = [];
@@ -8,9 +8,12 @@ $(document).ready(function() {
     var width = 600;
     var height = 600;
     var layer = new Konva.Layer();
+    var drawLine = [];
     var polyLot =[];
+    var infoLots =[];
+    var featureIDLot =0;
     var stage = new Konva.Stage({
-      container: 'viewportMid',
+      container: 'viewportTop',
       width: width,
       height: height
     });
@@ -22,6 +25,7 @@ $(document).ready(function() {
         var pixelX = [];
         var pixelY = [];
         var drawCounter = 0;
+
     });
     function appendLog(message) {
         var d = log[0];
@@ -59,9 +63,11 @@ $(document).ready(function() {
         make_base();
         pixelX = [];
         pixelY = [];
+        drawCounter = 0;
+        drawLine = [];
         document.getElementById("form_x").innerHTML = pixelX;
         document.getElementById("form_y").innerHTML = pixelY;
-        drawCounter = 0;
+
         if (!conn) {
             return false;
         }
@@ -75,13 +81,14 @@ $(document).ready(function() {
         conn.addEventListener('message', function(e){
             var msgServer=JSON.parse(e.data);
 
-            console.log(msgServer);
+            // console.log(msgServer);
 
             var msgType = msgServer.messageprocessedtype;
             var msgOriginal;
             var msgOriginal1DArray=[];
             var msgOriginal2DArray=[];
             var msgProcessed;
+            var infoLot=[];
             if (msgType==0){
                 msgOriginal = msgServer.lot2client[0].messageprocessedoriginal;
                 msgProcessed = msgServer.lot2client[0].messageprocessedvectors;
@@ -99,21 +106,16 @@ $(document).ready(function() {
                     msgProcessed = msgServer.lot2client[i].messageprocessedvectors;
 
                     msgOriginal2DArray.push(msgOriginal);
-
-                    appendLog($("<div/>").text(">  Pixel Coor: "+
-                           JSON.stringify(msgOriginal)));
-                        appendLog($("<div/>").text(" Latitude: "+
-                               JSON.stringify(msgProcessed.Latitude).concat(" ,Longtitude: "+
-                               JSON.stringify(msgProcessed.Longtitude).concat(" ,Elevation: "+
-                               JSON.stringify(msgProcessed.Elevation)))));
-                        // console.log(msgOriginal)
+                    infoLot.push(msgProcessed.Latitude,msgProcessed.Longtitude); 
                 }
+                for(var x = 0; x < msgOriginal2DArray.length; x++){
+                    msgOriginal1DArray = msgOriginal1DArray.concat(msgOriginal2DArray[x]);
+                }
+                infoLots.push(infoLot);
+                drawLots(msgOriginal1DArray,featureIDLot,infoLots[featureIDLot]);
+                featureIDLot++;
             }
 
-            for(var i = 0; i < msgOriginal2DArray.length; i++){
-               msgOriginal1DArray = msgOriginal1DArray.concat(msgOriginal2DArray[i]);
-            }
-            drawLots(msgOriginal1DArray);
             make_base();
         });
 
@@ -135,53 +137,81 @@ $(document).ready(function() {
         };
     }
 
-    function getMousePos(canvas, evt) {
-        canvas = document.getElementById('viewportTop');
-        var rect = canvas.getBoundingClientRect();
-        return {
-          x: evt.clientX - rect.left,
-          y: evt.clientY - rect.top
-        };
-    }
-
-    function draw(e) {
+    $("#viewportTop").click(function(e){
+ 
+        var parentOffset = $(this).offset(); 
+        var relX = e.pageX - parentOffset.left;
+        var relY = e.pageY - parentOffset.top;
+        
+        
         if (drawCounter <4){
-            var canvas = document.getElementById('viewportTop');
-            context = canvas.getContext('2d');
-            var pos = getMousePos(canvas, e);
-            pixelX.push(pos.x);
-            pixelY.push(pos.y);
+            pixelX.push(relX);
+            pixelY.push(relY);
             document.getElementById("form_x").innerHTML = pixelX;
             document.getElementById("form_y").innerHTML = pixelY;
-            context.fillStyle = "white";
-            context.beginPath();
-            context.arc(pos.x, pos.y, 2, 0, 2*Math.PI);
-            context.fill();
             drawCounter++;
+            drawClick(relX,relY);
+
+            drawLine.push(relX,relY);
+            var featureIDLot=0;
+            var infoLots =0;
+            drawLots(drawLine,featureIDLot,infoLots);
+            
+
         }
         else{
             appendLog($("<div>Please send the pixel array for processing.<\/div>"));
+            drawLine = [];
         }
+     });
+
+
+    function drawClick(relX,relY) {
+        var circle = new Konva.Circle({
+            x: relX,
+            y: relY,
+            radius: 3,
+            fill: 'white',
+            stroke: 'white',
+            strokeWidth: 1
+          });
+
+          layer.add(circle);
+          stage.add(layer);
     }
 
-    drawLots();
-    function drawLots(msgOriginal1DArray) {
+    function drawLots(msgOriginal1DArray,featureIDLot, infoLot) {
+
+    var closeLot = false;
+    if (msgOriginal1DArray.length >=8){
+        closeLot = true;
+    }
     polyLot = new Konva.Line({
         points: msgOriginal1DArray,
         stroke: '#edeaea',
         strokeWidth: 5,
-        closed : true
+        closed : closeLot
       });
 
-    polyLot.on('mouseover', function () {
-        appendLog($("<div/>").text("in"));
-      });
+    polyLot.on('click', function () {
+
+    appendLog($("<div/>").text("> Coordinates of Lot Corners: "));
+    appendLog($("<div/>").text(" Point 1 <==> Latitude: "+
+        JSON.stringify(infoLot[0]).concat(" ,Longtitude: "+
+        JSON.stringify(infoLot[1]))));
+    appendLog($("<div/>").text(" Point 2 <==> Latitude: "+
+        JSON.stringify(infoLot[2]).concat(" ,Longtitude: "+
+        JSON.stringify(infoLot[3]))));
+    appendLog($("<div/>").text(" Point 3 <==> Latitude: "+
+        JSON.stringify(infoLot[4]).concat(" ,Longtitude: "+
+        JSON.stringify(infoLot[5]))));
+    appendLog($("<div/>").text(" Point 4 <==> Latitude: "+
+        JSON.stringify(infoLot[6]).concat(" ,Longtitude: "+
+        JSON.stringify(infoLot[7]))));
+
+    });
 
     layer.add(polyLot);
     stage.add(layer);
     }
-    
-    window.draw = draw;
-
-
 });
